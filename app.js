91ae5812a5eb1c54556ab3712c0dac951d33fdf8
zackscovitch01 <bagoudoumoubarack@icloud.com>
2024-04-30 06:08:58 -0400

    Gallery Object

--- app.js
+++ app.js
@@ -7,3 +7,32 @@ function getElement(selection) {
     `Please check "${selection}" selector, no such element exists`
   );
 }
+
+function Gallery(element) {
+  this.container = element;
+  this.list = [...element.querySelectorAll(".img")];
+  // target
+  this.modal = getElement(".modal");
+  this.modalImg = getElement(".main-img");
+  this.modalImages = getElement(".modal-images");
+  this.closeBtn = getElement(".close-btn");
+  this.nextBtn = getElement(".next-btn");
+  this.prevBtn = getElement(".prev-btn");
+  // container event
+  this.container.addEventListener(
+    "click",
+    function (e) {
+      if (e.target.classList.contains("img")) {
+        this.openModal(e.target, this.list);
+      }
+    }.bind(this)
+  );
+}
+
+Gallery.prototype.openModal = function (selectedImage, list) {
+  console.log("open modal");
+  this.modal.classList.add("open");
+};
+
+nature = new Gallery(getElement(".nature"));
+city = new Gallery(getElement(".city"));
