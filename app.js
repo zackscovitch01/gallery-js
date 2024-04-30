ae8b4e18242f6b288d20eafe00a18fd72436290a
zackscovitch01 <bagoudoumoubarack@icloud.com>
2024-04-30 06:40:00 -0400

    setMainImage + show others images in the modal

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ function Gallery(element) {
   // target
   this.modal = getElement(".modal");
   this.modalImg = getElement(".main-img");
+  this.imageName = getElement(".image-name");
   this.modalImages = getElement(".modal-images");
   this.closeBtn = getElement(".close-btn");
   this.nextBtn = getElement(".next-btn");
@@ -30,8 +31,20 @@ function Gallery(element) {
 }
 
 Gallery.prototype.openModal = function (selectedImage, list) {
-  console.log("open modal");
+  this.setMainImage(selectedImage);
+  this.modalImages.innerHTML = list
+    .map(function (image) {
+      return `<img src="${
+        image.src
+      }" title="${image.title}" data-id="${image.dataset.id}" class="${selectedImage.dataset.id === image.dataset.id ? "modal-img selected" : "modal-img"}"/>`;
+    })
+    .join("");
   this.modal.classList.add("open");
+};
+
+Gallery.prototype.setMainImage = function (selectedImage) {
+  this.modalImg.src = selectedImage.src;
+  this.imageName.textContent = selectedImage.title;
 };
 
 nature = new Gallery(getElement(".nature"));
